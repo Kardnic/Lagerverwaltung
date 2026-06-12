fbeb6d968a259202bcb8cee902aa44011b5b5b88
Kardnic <158296649+Kardnic@users.noreply.github.com>
2026-06-12 05:54:32 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCnux0f6ekyZMrxoHyJvcKWxvr1udZXN2w",
+  authDomain: "lagerverwaltung-1673d.firebaseapp.com",
+  projectId: "lagerverwaltung-1673d",
+  storageBucket: "lagerverwaltung-1673d.firebasestorage.app",
+  messagingSenderId: "314142654599",
+  appId: "1:314142654599:web:8458f7d72d831df5c0f822",
+  measurementId: "G-DPP87G4YR5"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
