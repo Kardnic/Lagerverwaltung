810e8c4aa565841acf3c9611db742333ce839976
Kardnic <158296649+Kardnic@users.noreply.github.com>
2026-07-09 12:12:20 +0200

    Add Lager-Konfiguration to config.js

---
+++ js/config.js
@@ -0,0 +1,42 @@
+// ================================
+// Lager-Konfiguration
+// ================================
+
+const CONFIG = {
+
+    bereiche: [
+        {
+            id: "regal1",
+            name: "Regal 1",
+            plaetze: 15
+        },
+        {
+            id: "regal2",
+            name: "Regal 2",
+            plaetze: 48
+        },
+        {
+            id: "regal3",
+            name: "Regal 3",
+            plaetze: 48
+        },
+        {
+            id: "regal4",
+            name: "Regal 4",
+            plaetze: 36
+        },
+        {
+            id: "feld2",
+            name: "Feld 2",
+            plaetze: 16
+        },
+        {
+            id: "feld3",
+            name: "Feld 3",
+            plaetze: 16
+        }
+    ],
+
+    firestoreCollection: "lager"
+
+};
