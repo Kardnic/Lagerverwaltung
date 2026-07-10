dc1a577220f403a1ae18ecfecdcf7d0ac5af1c17
Kardnic <158296649+Kardnic@users.noreply.github.com>
2026-07-10 05:45:22 +0200

    Remove Lager-Konfiguration from config.js
    Removed Lager-Konfiguration and related configuration for bereiche.

--- js/config.js
+++ js/config.js
@@ -36,44 +36,6 @@ const CONFIG = {
             plaetze: 16
         }
     ],
-    // ================================
-// Lager-Konfiguration
-// ================================
-
-const CONFIG = {
-
-    bereiche: [
-        {
-            id: "regal1",
-            name: "Regal 1",
-            plaetze: 15
-        },
-        {
-            id: "regal2",
-            name: "Regal 2",
-            plaetze: 48
-        },
-        {
-            id: "regal3",
-            name: "Regal 3",
-            plaetze: 48
-        },
-        {
-            id: "regal4",
-            name: "Regal 4",
-            plaetze: 36
-        },
-        {
-            id: "feld2",
-            name: "Feld 2",
-            plaetze: 16
-        },
-        {
-            id: "feld3",
-            name: "Feld 3",
-            plaetze: 16
-        }
-    ],
 
     maschinen: [
         "K20",
@@ -85,10 +47,6 @@ const CONFIG = {
         "K26",
         "U27"
     ],
-
-    firestoreCollection: "lager"
-
-};
 
     firestoreCollection: "lager"
 
