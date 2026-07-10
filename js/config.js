ec5db67e1c6c300d164b80011286036395d85d97
Kardnic <158296649+Kardnic@users.noreply.github.com>
2026-07-10 05:44:50 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -36,6 +36,59 @@ const CONFIG = {
             plaetze: 16
         }
     ],
+    // ================================
+// Lager-Konfiguration
+// ================================
+
+const CONFIG = {
+
+    bereiche: [
+        {
+            id: "regal1",
+            name: "Regal 1",
+            plaetze: 15
+        },
+        {
+            id: "regal2",
+            name: "Regal 2",
+            plaetze: 48
+        },
+        {
+            id: "regal3",
+            name: "Regal 3",
+            plaetze: 48
+        },
+        {
+            id: "regal4",
+            name: "Regal 4",
+            plaetze: 36
+        },
+        {
+            id: "feld2",
+            name: "Feld 2",
+            plaetze: 16
+        },
+        {
+            id: "feld3",
+            name: "Feld 3",
+            plaetze: 16
+        }
+    ],
+
+    maschinen: [
+        "K20",
+        "K21",
+        "K22",
+        "K23",
+        "K24",
+        "U25",
+        "K26",
+        "U27"
+    ],
+
+    firestoreCollection: "lager"
+
+};
 
     firestoreCollection: "lager"
 
